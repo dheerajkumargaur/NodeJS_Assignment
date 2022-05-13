c4f532faae9509202505ebc3a8a06e90dfd2c88b
Dheeraj Kumar Gaur <93270093+dheerajkumargaur@users.noreply.github.com>
2022-05-13 16:50:43 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const app = express()
 const cookieParser = require("cookie-parser");
-const errorMiddleware = require("./middlewares/error")
 
 app.use(express.json())
 app.use(cookieParser())
 
 //Routes Import
+const psychiatrist = require("./routes/hospitalRoute")
 const psychiatrist = require("./routes/psychiatristRoute")
 const patient = require("./routes/patientRoute")
 
-app.use("/api/v1",psychiatrist)
-app.use("/api/v1",patient)
+app.use("/r1",Hospital)
+app.use("/r1",psychiatrist)
+app.use("/r1",patient)
 
 
 //mddileware for Errors
